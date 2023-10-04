b5d4b5e4431ed96940f25d267949b83a7a262deb
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2023-10-03 22:18:53 -0700

    commented code

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Put script on doors
 public class Door : MonoBehaviour
 {
+    //enter name of scene you want to enter in editor
     public string room;
 
     public void enterDoor ()
     {
+        //loads scene with name equal to room
         SceneManager.LoadScene(room);
     }
 }
--- Assets/Scripts/Entities/Player/PlayerInteract.cs
+++ Assets/Scripts/Entities/Player/PlayerInteract.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
-    public bool canInteract = false;
+    //checks if you are over something interactable
+    private bool canInteractWithDoor = false;
     
+    //what door you can interact with
     Door door = null;
 
     void Update ()
     {
-        if((Input.GetKeyDown(KeyCode.F)) && (canInteract == true))
+        //if you press f while you can interact with a door
+        if((Input.GetKeyDown(KeyCode.F)) && (canInteractWithDoor == true))
         {
+            //activates enterDoor method associated with door and loads new scene corresponding with door
             door.enterDoor();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //if you entered a trigger with tag "Door", makes player able to interact with door, and gets door script attached to door
         if(collision.gameObject.tag == "Door")
         {
-            canInteract = true;
+            canInteractWithDoor = true;
 
             door = collision.GetComponent<Door>();
         }
@@ -28,9 +33,10 @@ public class PlayerInteract : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        //if you exit a trigger with tag "Door", no longer able to interact with door, and sets door able to interact with to null
         if(collision.gameObject.tag == "Door")
         {
-            canInteract = false;
+            canInteractWithDoor = false;
 
             door = null;
         }
