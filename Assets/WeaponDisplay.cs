bfb77b2207ebdc4a0e4fa3b96af93b2a5a5a1e69
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-10-05 22:00:05 -0700

    Template of Weapon Selector

---
+++ Assets/WeaponDisplay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WeaponDisplay : MonoBehaviour
+{
+    public Image weaponImage;
+    public Image weaponImage2;
+    public Image weaponImage3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
