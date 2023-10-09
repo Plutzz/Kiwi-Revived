137f67a08f81456f854a04de62739bf2c99b0b55
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-10-08 18:03:20 -0700

    Created Script for InventoryDisplay

--- Assets/ShopGrid.cs
+++ Assets/ShopGrid.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class ShopGrid : MonoBehaviour
 {
 
@@ -10,7 +11,7 @@ public class ShopGrid : MonoBehaviour
     public GameObject[] shopItems; // Sprites of the items/abilities/cost that can be bought
     public GameObject gridElement;
     public GridLayoutGroup grid;
-
+    public string room;
     void Start()
     {
         for (int i = 0; i < shopItems.Length; i++)
@@ -18,12 +19,16 @@ public class ShopGrid : MonoBehaviour
             GameObject element = Instantiate(gridElement, transform.position, Quaternion.identity);
             element.transform.SetParent(grid.transform);
             element.transform.localScale = new Vector3(1, 1, 1);
+            
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SceneManager.LoadScene(room);
+        }
     }
 }
