1d3b65859bd1edd81d2771e6967ea695d77645c4
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2023-11-05 18:01:41 -0800

    moved kain to main scene

--- Assets/FireBolt.cs
+++ Assets/FireBolt.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FireBolt : MonoBehaviour
 {
+    public new CircleCollider2D collider;
+    public new ParticleSystem particleSystem;
+    public SpriteRenderer spriteRenderer;
     public int damage = 5;
     public float velocity = 10.0f;
     [SerializeField] private int playerScriptsChildNumber = 5;
@@ -11,7 +14,7 @@ public class FireBolt : MonoBehaviour
 
     void Start ()
     {
-        Destroy(this.gameObject, 6f);
+        Destroy(this.gameObject, 5f);
     }
 
     void Update ()
@@ -20,9 +23,21 @@ public class FireBolt : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("nohit");
         if(other.gameObject.CompareTag("Player"))
         {
             other.gameObject.transform.GetChild(playerScriptsChildNumber).GetComponent<PlayerHealth>().takeDamage(damage);
+            Extinguish();
+            Debug.Log("hit");
         }
+        Debug.Log("nohit2");
+    }
+
+    public void Extinguish()
+    {
+        collider.isTrigger = false;
+        particleSystem.Stop();
+        spriteRenderer.color = Color.blue;
+        gameObject.layer = LayerMask.NameToLayer("Ground");
     }
 }
